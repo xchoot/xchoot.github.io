6c707371007381990da25985dcfb9584e705d26a
xchoot <106496026+xchoot@users.noreply.github.com>
2022-06-10 22:08:01 +1000

    Delete j_delivery.js

--- j_delivery.js
+++
@@ -1,8 +0,0 @@
-function pickupCheck() {
-    document.getElementsByClassName("shipping").style.visibility = "grey"
-}
-
-function shipCheck() {
-    document.getElementsByClassName("shipping").style.visibility = "black"
-}
-    
