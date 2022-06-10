0e67fabe63897745d092d728d20ec3c134ba8f45
xchoot <106496026+xchoot@users.noreply.github.com>
2022-06-10 22:09:36 +1000

    Delete j_search.js

--- j_search.js
+++
@@ -1,7 +0,0 @@
-function printInput(searchResult) {
-    let print = document.getElementById("search").value
-    let searchResult = "SEARCHED FOR: " + print
-    console.log(searchResult)
-
-    return searchResult
-}
