727db8592776dd1806ba42e11412692c8727b363
xchoot <106496026+xchoot@users.noreply.github.com>
2022-06-10 20:52:08 +1000

    Add files via upload

---
+++ j_delivery.js
@@ -0,0 +1,8 @@
+function pickupCheck() {
+    document.getElementsByClassName("shipping").style.visibility = "grey"
+}
+
+function shipCheck() {
+    document.getElementsByClassName("shipping").style.visibility = "black"
+}
+    
